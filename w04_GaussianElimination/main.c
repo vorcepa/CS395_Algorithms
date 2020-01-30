6f8e90df372b3dba98e256e06bed530ad02f91a2
Phillip <vorcepa@gmail.com>
2020-01-30 13:33:05 -0800

    Completed.  Prints the matrix according to the requirements

--- w04_GaussianElimination/main.c
+++ w04_GaussianElimination/main.c
@@ -1,65 +1,113 @@
+/*********************************************
+ * Id: vorc2804
+ *
+ * This program takes in arguments from the user that represent the coefficients
+ * of an augmented matrix for some linear system of unknown variables.
+ * It then performs Gaussian Elimination on the matrix, but does not determine
+ * consistency or the span of the matrix.
+ *********************************************/
 #include "stdio.h"
 #include "stdlib.h"
 #include "float.h"
 
-#define DBL_NULL DBL_MAX
+#define FLT_NULL FLT_MAX
 
-void print_matrix();
+void print_matrix(int, float**);
+void doGaussianElim(int, float**);
 
+/*
+ * Driver function for the program.  The user supplies the number of rows the
+ * matrix is to have, followed by the constants in each position of the
+ * augmented matrix.
+ * 
+ * @param argc: the number of arguments passed in by the user
+ * @param argv[0]: the name of the executable that runs this file
+ * @param argv[1]: the number of rows in the matrix
+ * @param argv[2..argc - 1]: coefficients in the matrix
+ */
 int main(int argc, char* argv[]){
-    int numEquations = atoi(argv[1]);                                               /* number of rows in matrix */
-    int invisCoefficients = (argc - 2) % numEquations;                              /* number of zeroes to add to the final row if the number of arguments / number of rows is not an integer value */
-    int numCoefficients = argc - 2 - numEquations + invisCoefficients;              /* number of coefficients (LHS of linear system) */
-    int coefficientsPerRow = ((argc - 2 + invisCoefficients) / numEquations) - 1;   
-    double* constants = malloc((numEquations + 1)*sizeof(double));                  /* reserve in memory the number of constants (equal to number of equations) (RHS of linear system) */
-    double** coefficientMatrix = malloc(numEquations * sizeof(double*));            /* reserve in memory a 2D array of floats that will be the coefficient matrix */
-    constants[numEquations] = DBL_NULL;
+   int numRows = atoi(argv[1]);                                    /* number of rows in matrix */
+   int numConstants = argc - 2;
+   int coefficientsPerRow = numConstants / numRows;   
+   float** matrix = malloc((numRows + 1) * sizeof(float*));  /* reserve in memory a 2D array of floats that will be the coefficient matrix */
+   matrix[numRows] = NULL;
 
-    int i;
-    int j = 0;
-    int k = 0;
-    int offset = 0;
-    for (i = 2; i + offset < argc; i++){
-        if (j == 0){
-            coefficientMatrix[k] = malloc((coefficientsPerRow + 1) * sizeof(double));
-            k++;
-        }
-        
-        coefficientMatrix[k - 1][j] = atof(argv[i + offset]);
-        j++;
+   int i;
+   int column = 0;
+   int row = 0;
+   for (i = 2; i < argc; i++){
+      if (column == 0){
+         matrix[row] = malloc((coefficientsPerRow + 1) * sizeof(float));
+         matrix[row][coefficientsPerRow] = FLT_NULL;
+         row++;
+      }
 
-        if (j % coefficientsPerRow == 0){
-            coefficientMatrix[k - 1][j] = DBL_NULL;
-            j = 0;
-            offset++;
-        }
-    }
+      matrix[row - 1][column] = atof(argv[i]);
+      column++;
 
-    int m;
-    int n;
-    printf("[");
-    for (m = 0; m < numEquations; m++){
-        n = 0;
-        while (coefficientMatrix[m][n] != DBL_NULL){
-            if (m > 0 && n == 0){
-                printf(" %.2lf ", coefficientMatrix[m][n]);
-            }
-            else{
-                printf("%.2lf ", coefficientMatrix[m][n]);
-            }
-            n++;
-        }
-        if (m != numEquations - 1){
-            printf("\n");
-        }
-    }
-    printf("]\n");
+      if (column % coefficientsPerRow == 0){
+         column = 0;
+      }
+   }
 
+   doGaussianElim(numRows, matrix);
 
-    free(constants);
-    for (i = 0; i < k; i++){
-        free(coefficientMatrix[i]);
-    }
-    free(coefficientMatrix);
-    return 0;
+   for (i = 0; i < numRows + 1; i++){
+      free(matrix[i]);
+   }
+   free(matrix);
+   return 0;
+}
+
+/*
+ * Performs the Gaussian Elimination on the matrix that was created
+ * from the parameters passed in at run time.  Does not return as
+ * the matrix is stored on the heap as an array of pointers.
+ * 
+ * @param numRows: the number of rows in the matrix passed in
+ * @param matrix: the coefficient matrix (augmented) that we are performing 
+ * the Gaussian Elimination on
+ */
+void doGaussianElim(int numRows, float** matrix){
+   int i, j, k;
+   float save;
+
+   print_matrix(numRows, matrix);
+
+   for (i = 0; i < numRows - 1; i++){
+      for (j = i + 1; j < numRows; j++){
+         save = matrix[j][i];
+         for (k = i; k < numRows + 1; k++){
+            matrix[j][k] = matrix[j][k] - matrix[i][k] * save / matrix[i][i];
+         }
+      }
+      print_matrix(numRows, matrix);
+   }
+}
+
+/*
+ * Prints the matrix.  This is called before the matrix is modified,
+ * and then at each step of the Gaussian Elimination process (including
+ * the finished state).
+ * 
+ * @param numRows: the number of rows in the matrix
+ * @param matrix: the matrix to be printed
+ */
+void print_matrix(int numRows, float** matrix){
+   int m;
+   int n;
+
+   for (m = 0; matrix[m] != NULL; m++){
+      n = 0;
+      while (matrix[m][n] != FLT_NULL){
+         if (matrix[m][n + 1] == FLT_NULL){
+            printf("%.2lf \n", matrix[m][n]);
+         }
+         else{
+            printf("%.2lf ", matrix[m][n]);
+         }
+         n++;
+      }
+   }
+   printf("\n");
 }
